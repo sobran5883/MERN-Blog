c02dce8fba070afe17274e4de402c93523aa1ee9
sobran rajak <sobran0621@gmail.com>
2024-03-05 12:49:35 +0530

    added nav button outline feature

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,7 +24,7 @@ function Header() {
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+          <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
         </Link>
         <Navbar.Toggle/>
       </div>
