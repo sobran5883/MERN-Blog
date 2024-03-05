a1c3507a31dec84c949a976ade3796fb5ff57d21
sobran rajak <sobran0621@gmail.com>
2024-03-05 18:23:38 +0530

    add middleware and hashed stored password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from '../models/user.model.js'; 
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next)=>{
     const {username, email, password} = req.body;
     if(!username || !email || !password || username==='' || email==='' || password===''){
-        return res.status(400).json({message:'All fields are required'});
+        next(errorHandler(400, 'All fields are required'));
     }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password:hashedPassword});
@@ -12,6 +13,6 @@ export const signup = async (req, res, next)=>{
         await newUser.save();
         res.status(201).json("User created successfully")
     } catch (error) {
-        res.status(500).json({message: error.message});
+        next(error);
     }
 }
